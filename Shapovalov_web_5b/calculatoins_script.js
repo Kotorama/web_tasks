28ec732f09e4d5cbdd64f01ac2cf9e22cc7e26cb
“Kotorama” <michael.shapovalov3012@gmail.com>
2023-11-23 19:54:35 +0200

    web 5b

--- Shapovalov_web_5b/calculatoins_script.js
+++ Shapovalov_web_5b/calculatoins_script.js
@@ -10,7 +10,7 @@ function calcHeron(a, b, c) {
     return Math.ceil(area);
 }
 
-function calcArea(a, b, g) {
+function calcAreaTwoSides(a, b, g) {
     console.log(a, b, g);
     area = a*b*(Math.sin(Math.PI * (g/180)) / 2);  //Math.PI * (g/180) converts degrees into radians
     console.log()
@@ -18,54 +18,66 @@ function calcArea(a, b, g) {
     return Math.round((area + Number.EPSILON) * 100) / 100;  //(area + Number.EPSILON) makes sure that things like 1.005 round correctly
 }
 
+function calcAreaHeight(b, h) {
+    console.log(b, h)
+    area = (b*h)/2
+    console.log(area)
+    return Math.ceil(area)
+}
+
 function calculate() {
     console.log('calculate works')
-    if (document.getElementsByClassName('selected')[0].id === 'task1-button') {
-        a = parseInt(document.getElementById('edge1').value);
-        b = parseInt(document.getElementById('edge2').value);
-        c = parseInt(document.getElementById('edge3').value);
-        document.getElementById('result-field').innerText = calcHeron(a, b, c).toString();
-    }
-    if (document.getElementsByClassName('selected')[0].id === 'task2-button') {
-        a = parseInt(document.getElementById('side1').value);
-        b = parseInt(document.getElementById('side2').value);
-        g = parseInt(document.getElementById('angle').value);
-        document.getElementById('result-field').innerText = calcArea(a, b, g).toString();
+    switch (document.querySelector('input[name=menu-button]:checked').id) {
+        case 'task1-button':
+            a = parseInt(document.getElementById('edge1').value);
+            b = parseInt(document.getElementById('edge2').value);
+            c = parseInt(document.getElementById('edge3').value);
+            document.getElementById('result-field').innerText = calcHeron(a, b, c).toString();
+            break;
+
+        case 'task2-button':
+            a = parseInt(document.getElementById('side1').value);
+            b = parseInt(document.getElementById('side2').value);
+            g = parseInt(document.getElementById('angle').value);
+            document.getElementById('result-field').innerText = calcAreaTwoSides(a, b, g).toString();
+            break;
+
+        case 'task3-button':
+            b = parseInt(document.getElementById('base').value);
+            h = parseInt(document.getElementById('height').value);
+            document.getElementById('result-field').innerText = calcAreaHeight(b, h).toString();
+            break;
     }
 }
 
 function calcChoice(elementId) {
-    if (document.getElementsByClassName('selected').length > 0){
-        document.getElementsByClassName('selected')[0].classList.remove('selected')
-    }   
-    if (elementId === 'task1-button') {
-        document.getElementById(elementId).classList.add('selected')
-        console.log('it happened')
-        document.getElementById('calc-cont').innerHTML = ' ';
-        document.getElementById('calc-cont').innerHTML = ` 
-            <div class="input-wrapper">
-                <p class="input-title">AB</p>
-                <input type="number" min="0" step="1" id="edge1"/>
-            </div>
-            <div class="input-wrapper">
-                <p class="input-title">BC</p>
-                <input type="number" min="0" step="1" id="edge2"/>
-            </div>
-            <div class="input-wrapper">
-                <p class="input-title">CD</p>
-                <input type="number" min="0" step="1" id="edge3"/>
-            </div>
-            <div class="calc-result">
-                <p class='result-title'>Result:</p>
-                <p id="result-field"> </p>
-            </div>
-            <button class="calc-button" id='calc-button' onclick="calculate()">Calculate</button>
-    `;
-    }
-    if (elementId === 'task2-button') {
-        document.getElementById(elementId).classList.add('selected')
-        console.log('it kjkh')
-        document.getElementById('calc-cont').innerHTML = ' ';
+    switch (elementId) {
+        case 'task1-button' :
+            console.log('it happened')
+            document.getElementById('calc-cont').innerHTML = ' ';
+            document.getElementById('calc-cont').innerHTML = ` 
+                <div class="input-wrapper">
+                    <p class="input-title">AB</p>
+                    <input type="number" min="0" step="1" id="edge1"/>
+                </div>
+                <div class="input-wrapper">
+                    <p class="input-title">BC</p>
+                    <input type="number" min="0" step="1" id="edge2"/>
+                </div>
+                <div class="input-wrapper">
+                    <p class="input-title">CD</p>
+                    <input type="number" min="0" step="1" id="edge3"/>
+                </div>
+                <div class="calc-result">
+                    <p class='result-title'>Result:</p>
+                    <p id="result-field"> </p>
+                </div>
+                <button class="calc-button" id='calc-button' onclick="calculate()">Calculate</button>
+                `;
+        break;
+
+        case 'task2-button' :
+            document.getElementById('calc-cont').innerHTML = ' ';
         document.getElementById('calc-cont').innerHTML = ` 
             <div class="input-wrapper">
             <p class="input-title">Side A</p>
@@ -84,12 +96,35 @@ function calcChoice(elementId) {
                 <p id="result-field"> </p>
             </div>
             <button class="calc-button" id='calc-button' onclick="calculate()">Calculate</button>
-    `;
-    
-    document.getElementById('calc-button').addEventListener('click', function () { calculate})
+            `;
+        break;
+
+        case 'task3-button' :
+            document.getElementById('calc-cont').innerHTML = ' ';
+            document.getElementById('calc-cont').innerHTML = ` 
+            <div class="input-wrapper">
+                <p class="input-title">Base B</p>
+                <input type="number" min="0" step="1" id="base"/>
+            </div>
+            <div class="input-wrapper">
+                <p class="input-title">Height H</p>
+                <input type="number" min="0" step="1" id="height"/>
+            </div>
+            <div class="calc-result">
+                    <p class='result-title'>Result:</p>
+                    <p id="result-field"> </p>
+            </div>
+            <button class="calc-button" id='calc-button' onclick="calculate()">Calculate</button>`;
+        break; 
     }
-    
+    document.getElementById('calc-button').addEventListener('click', function () { calculate})
 }
 
-document.getElementById('task1-button').addEventListener('click', function () { calcChoice('task1-button') })
-document.getElementById('task2-button').addEventListener('click', function () { calcChoice('task2-button') })
+
+
+
+document.querySelectorAll('input[name=menu-button]').forEach(element => {
+    element.addEventListener('click', () => {
+        calcChoice(document.querySelector('input[name=menu-button]:checked').id)
+    })
+}); 
