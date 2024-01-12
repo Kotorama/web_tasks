8d857b82425eacbcb6c615c4a81c36a9ed5eec8c
“Kotorama” <michael.shapovalov3012@gmail.com>
2024-01-12 07:25:17 +0200

    idk man

---
+++ shopping-kart-project/snickers-online-shop/src/pages/cart/cart-item.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart-context';
+import { backspace } from 'phosphor-react'
+
+
+export const CartItem = (props) => {
+  const { id, title, price, imageSrc, brand } = props.data;
+  const { addToCart, removeFromCart, cartItems } = useContext(CartContext);
+  return (
+    <div className='cart-item'>
+      <div>
+        <img src={imageSrc}></img>
+      </div>
+      <div className='info'>
+        <div className='item-title'>{title}</div>
+        <div className='price'>{cartItems[id] > 1 && <>({cartItems[id]})</>}{price * cartItems[id]} UAH</div>
+      </div>
+      <div className='buttons'>
+        <button className='add-button'></button>
+        <button className='remove-button'></button>
+      </div>
+    </div>
+  )
+}
--- shopping-kart-project/snickers-online-shop/src/pages/cart/cart.js
+++ shopping-kart-project/snickers-online-shop/src/pages/cart/cart.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import productsData from '../../products.json'
+import { CartContext } from '../../context/cart-context';
+import { useContext } from 'react';
+import { CartItem } from './cart-item'
+
 
 export const Cart = () => {
-  return <div>cart</div>;
+  const { addToCart, removeFromCart, cartItems } = useContext(CartContext);
+  return <div className='cart'>
+    <div>
+      <h1>Your cart</h1>
+    </div>
+    <div className='cartItems'>
+      {productsData.map((product) => {
+        if (cartItems[product.id] > 0) {
+          return <CartItem data={product} />
+        }
+      })}
+    </div>
+  </div>;
 };
