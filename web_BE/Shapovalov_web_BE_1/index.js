d3d3a926b39928c0bf7e3351451602972e231084
Kotorama <michael.shapovalov3012@gmail.com>
2024-02-27 20:08:09 +0200

    pract 1 complete

--- web_BE/Shapovalov_web_BE_1/index.js
+++ web_BE/Shapovalov_web_BE_1/index.js
@@ -13,70 +13,154 @@ app.use(bodyParser.json());
  * res -> output data
  */
 app.post('/users', (req, res) => {
- const { body } = req;
+  const { body } = req;
 
- console.log(`body`, JSON.stringify(body));
+  console.log(`body`, JSON.stringify(body));
 
- const isUserExist = USERS.some(el => el.login === body.login);
- if (isUserExist) {
-  return res.status(400).send({ message: `user with login ${body.login} already exists` });
- }
+  const isUserExist = USERS.some(el => el.login === body.login);
+  if (isUserExist) {
+    return res.status(400).send({ message: `user with login ${body.login} already exists` });
+  }
 
- USERS.push(body);
+  USERS.push(body);
 
- res.status(200).send({ message: 'User was created' });
+  res.status(200).send({ message: 'User was created' });
 });
 
 app.get('/users', (req, res) => {
- const users = USERS.map(user => {
-  const { password, ...other } = user;
-  return other;
- });
- return res
-  .status(200)
-  .send(users);
+  const users = USERS.map(user => {
+    const { password, ...other } = user;
+    return other;
+  });
+  return res
+    .status(200)
+    .send(users);
 });
 
 app.post('/login', (req, res) => {
- const { body } = req;
+  const { body } = req;
 
- const user = USERS
-  .find(el => el.login === body.login && el.password === body.password);
+  const user = USERS
+    .find(el => el.login === body.login && el.password === body.password);
 
- if (!user) {
-  return res.status(400).send({ message: 'User was not found' });
- }
+  if (!user) {
+    return res.status(400).send({ message: 'User was not found' });
+  }
 
- const token = crypto.randomUUID();
+  const token = crypto.randomUUID();
 
- user.token = token;
- USERS.save(user.login, { token });
+  user.token = token;
+  USERS.save(user.login, { token });
 
- return res.status(200).send({
-  token,
-  message: 'User was login'
- });
+  return res.status(200).send({
+    token,
+    message: 'User was login'
+  });
 });
 
 app.post('/orders', authorizationMiddleware, (req, res) => {
- const { body, user } = req;
+  const { body, user } = req;
 
- const order = {
-  ...body,
-  login: user.login
- };
+  var price = Math.floor(Math.random() * (100 - 20 + 1) + 20);
 
- ORDERS.push(order);
+  const order = {
+    price: price.toString(),
+    ...body,
+    login: user.login
+  };
 
- return res.status(200).send({ message: 'Order was created', order });
+  ORDERS.push(order);
+
+  return res.status(200).send({ message: 'Order was created', order });
 });
 
 app.get('/orders', authorizationMiddleware, (req, res) => {
- const { user } = req;
+  const { user } = req;
 
- const orders = ORDERS.filter(el => el.login === user.login);
+  const orders = ORDERS.filter(el => el.login === user.login);
 
- return res.status(200).send(orders);
+  return res.status(200).send(orders);
+});
+
+app.get('/orders/from', authorizationMiddleware, (req, res) => {
+  const { user } = req;
+
+  const orders = ORDERS.filter(el => el.login === user.login);
+
+  // orders = orders.reverse();
+
+  let lastOrders = [];
+
+  for (var i = 0, len = orders.length; i < len; i++) {
+    if (!lastOrders.includes(orders[len - 1 - i].from)) {
+      lastOrders.push(orders[len - 1 - i].from);
+    };
+    if (lastOrders.length > 4) {
+      break
+    };
+  };
+  return res.status(200).send(lastOrders);
+});
+
+app.get('/orders/to', authorizationMiddleware, (req, res) => {
+  const { user } = req;
+
+  const orders = ORDERS.filter(el => el.login === user.login);
+
+  let lastOrders = [];
+
+  for (var i = 0, len = orders.length; i < len; i++) {
+    if (!lastOrders.includes(orders[len - 1 - i].to)) {
+      lastOrders.push(orders[len - 1 - i].to); // len - i для того щоб починати з кінця array без того
+      // щоб перевертати його, що може бути дорогою операцією при великих об'ємах даних
+    };
+    if (lastOrders.length > 4) {
+      break
+    };
+  };
+  return res.status(200).send(lastOrders);
+});
+
+app.get('/orders/highest', authorizationMiddleware, (req, res) => {
+  const { user } = req;
+
+  const orders = ORDERS.filter(el => el.login === user.login && el.price);
+
+  if (orders.length === 0) {
+    return res.status(400).send({ message: "User does not have orders yet" })
+  };
+
+  var max = 0;
+  var highest = null;
+  for (var i = 0, len = orders.length; i < len; i++) {
+    if (orders[i].price > max) {
+      max = orders[i].price;
+      highest = orders[i]
+    }
+  };
+
+  return res.status(200).send(highest);
+});
+
+app.get('/orders/lowest', authorizationMiddleware, (req, res) => {
+  const { user } = req;
+
+  const orders = ORDERS.filter(el => el.login === user.login && el.price);
+
+  if (orders.length === 0) {
+    return res.status(400).send({ message: "User does not have orders yet" })
+  };
+
+  var min = null;
+  var lowest = null;
+  for (var i = 0, len = orders.length; i < len; i++) {
+    if (min === null || orders[i].price < min) {
+      min = orders[i].price;
+      lowest = orders[i]
+    }
+  };
+
+  return res.status(200).send(lowest);
 });
 
 app.listen(8080, () => console.log('Server was started'));
--- web_BE/Shapovalov_web_BE_1/middlewares.js
+++ web_BE/Shapovalov_web_BE_1/middlewares.js
@@ -1,20 +1,20 @@
 import { USERS } from './db.js';
 
 export const authorizationMiddleware = (req, res, next) => {
- const { headers } = req;
+  const { headers } = req;
 
- const token = headers.authorization;
+  const token = headers.authorization;
 
- if (!token) {
-  return res.status(401).send({ message: "token was not found" });
- }
+  if (!token) {
+    return res.status(401).send({ message: "token was not found" });
+  }
 
- const user = USERS.find(el => el.token === token);
- if (!user) {
-  return res.status(400).send({ message: "user with this token was not found" });
- }
+  const user = USERS.find(el => el.token === token);
+  if (!user) {
+    return res.status(400).send(`message: User was not found by token: ${token}`);
+  }
 
- req.user = user;
+  req.user = user;
 
- next();
+  next();
 };
