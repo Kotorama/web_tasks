6c18f014f8ab35b3e9dbb9a73579f65a6934b6e2
Kotorama <michael.shapovalov3012@gmail.com>
2024-03-13 10:15:05 +0200

    first and second point

---
+++ web_BE/Shapovalov_web_BE_2/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { OrdersRouter, UsersRouter } from './routers/index.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(OrdersRouter);
+
+app.use(UsersRouter);
+
+
+app.listen(8080, () => console.log('Server was started'));
